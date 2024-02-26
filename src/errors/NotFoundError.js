26412af39616114bec60f8b91d3a1d2e8bbccdbe
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 04:46:49 +0100

    Update NotFoundError.js

--- src/errors/NotFoundError.js
+++ src/errors/NotFoundError.js
@@ -1,1 +1,5 @@
-
+// Creates a child class 'NotFoundError' of parent class 'Error'
+class 'NotFoundError' extends Error {
+  consructor(resourseType, id) {
+    super('$(resourseType} with id ${id} not found ');
+    this.name= "NotFoundError";
