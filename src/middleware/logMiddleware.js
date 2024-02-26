ca331d910b57345df864f8e349962e2a0077878f
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 07:36:10 +0100

    Update logMiddleware.js

--- src/middleware/logMiddleware.js
+++ src/middleware/logMiddleware.js
@@ -1,1 +1,15 @@
+import logger from "./utils/log.js";
 
+const log = (req, res, next) => {
+    const start = new Date();
+
+    next();
+
+    const ms = new Date - start;
+    logger.info(
+        `${req.method} ${req.originalUrl}. status: ${req.statuscode}. Duration: ${{ms} ms`,
+
+    )
+};
+
+export default log;  
