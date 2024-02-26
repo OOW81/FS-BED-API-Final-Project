66fdfbbe6b1e475c5751634b0fe868fbb5bbcdf2
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 08:40:33 +0100

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,1 +1,21 @@
+import { Router } from "express";
+import generateJwt from "../services/auth/generateJwt.js";
 
+const router = Router();
+
+router.post("/", async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const token = await generateJwt(username, password);
+
+        if (!token) {
+            res.status(401).json({ message: "Invalid credentials!" });
+        } else {
+            res.status(200).json({ message: "Succesfully logged in!", token });
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+export default router;
