a50652d12fe2c27b569a6ace5d248d03d91aa869
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 06:54:24 +0100

    Update errorHander.js

--- src/middleware/errorHander.js
+++ src/middleware/errorHander.js
@@ -1,1 +1,8 @@
+// Middleware for capturing unhandled errors, returns an error message - general.
 
+const errorHandler = (err, req, res, next) => {
+    console.error(err);
+    return res.status(500).json({ message: "Something went wrong!" }); 
+};
+
+export default errorHandler;
