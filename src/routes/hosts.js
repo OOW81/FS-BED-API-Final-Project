bf31cf6bf78495b316e973f7ffd377d0dd31ac18
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 08:33:25 +0100

    Update hosts.js

--- src/routes/hosts.js
+++ src/routes/hosts.js
@@ -1,1 +1,70 @@
+import express from "express";
+import getHosts from "../services/hosts/getHosts.js";
+import getHostById from "../services/hosts/getHostById.js";
+import createHost from "../services/hosts/createHost.js";
+import updateHostById from "../services/hosts/updateHostById.js";
+import deleteHost from "../services/hosts/deleteHost.js";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
+import authMiddleware from "../middleware/auth.js";
 
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+        const { name } = req.query;
+        const hosts = await getHosts(name);
+
+        res.json(hosts);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post("/", authMiddleware, async (req, res, next) => {
+    try {
+        const { username, password, name, email, phoneNumber, profilePicture, aboutMe } = req.body;
+        const newHost = await createHost(username, password, name, email, phoneNumber, profilePicture, aboutMe);
+
+        res.status(201).json(newHost);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const host = await getHostById(id);
+
+        res.status(200).json(host);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+router.put("/:id", authMiddleware, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { username, password, name, email, phoneNumber, profilePicture, aboutMe } = req.body;
+        const updatedHost = await updateHostById(id, { username, password, name, email, phoneNumber, profilePicture, aboutMe });
+
+        res.status(200).json(updatedHost);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deletedHostId = await deleteHost(id);
+
+        res.status(200).json({
+            message: `Host with id ${deletedHostId} was deleted.`
+        });
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+export default router;
