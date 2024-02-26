bf0dc68e638a2afe81d91389cc06bbe902af4041
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 11:30:27 +0100

    Create get users & get user by ID route

--- src/services/users/getUsers.js
+++ src/services/users/getUsers.js
@@ -1,1 +1,22 @@
+import { PrismaClient } from "@prisma/client";
 
+const getUsers = async (username, email) => {
+    const prisma = new PrismaClient();
+    const users = await prisma.user.findMany({
+        where: {
+            username: {
+                contains: username
+            },
+            email: {
+                contains: email
+            }
+        }
+    })
+
+    return users.map(user => {
+        const { password, ...rest } = user;
+        return rest;
+    })
+};
+
+export default getUsers;
