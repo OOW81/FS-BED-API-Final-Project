52223dd4419d5a5cf9f85e903e426f841af386d7
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 10:55:07 +0100

    fix: 'equals' instead of 'contains'

--- src/services/properties/getProperties.js
+++ src/services/properties/getProperties.js
@@ -1,1 +1,19 @@
+import { PrismaClient } from "@prisma/client";
 
+const getProperties = async (location, pricePerNight) => {
+    const prisma = new PrismaClient();
+    const properties = await prisma.property.findMany({
+        where: {
+            location: {
+                contains: location
+            },
+            pricePerNight: {
+                equals: pricePerNight
+            }
+        }
+    })
+
+    return properties;
+};
+
+export default getProperties;
