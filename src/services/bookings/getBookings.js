8106c616a197af6be7e9296c140ded925412388a
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 10:05:21 +0100

    Create the CRUD services for bookings

--- src/services/bookings/getBookings.js
+++ src/services/bookings/getBookings.js
@@ -1,1 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 
+const getBookings = async (userId) => {
+    const prisma = new PrismaClient();
+
+    const bookings = await prisma.booking.findMany({
+        where: {
+            userId: {
+                contains: userId
+            }
+        }
+    })
+
+    return bookings;
+};
+
+export default getBookings;
