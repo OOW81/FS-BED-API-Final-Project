dc1b20219b87f5026725e436d5a747bdbf6904a0
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-26 08:23:53 +0100

    Update amenities.js

--- src/routes/amenities.js
+++ src/routes/amenities.js
@@ -1,1 +1,68 @@
+import express from "express";
+import getAmenities from "../services/amenities/getAmenities.js";
+import getAmenityById from "../services/amenities/getAmenityById.js";
+import createAmenity from "../services/amenities/createAmenity.js";
+import updateAmenityById from "../services/amenities/updateAmenityById.js";
+import deleteAmenity from "../services/amenities/deleteAmenity.js";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
+import authMiddleware from "../middleware/auth.js";
 
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+        const amenities = await getAmenities();
+        res.json(amenities);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post("/", authMiddleware, async (req, res, next) => {
+    try {
+        const { name } = req.body;
+        const newAmenity = await createAmenity(name);
+
+        res.status(201).json(newAmenity);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const amenity = await getAmenityById(id);
+
+        res.status(200).json(amenity);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+router.put("/:id", authMiddleware, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        const updatedAmenity = await updateAmenityById(id, { name });
+
+        res.status(200).json(updatedAmenity);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deletedAmenityId = await deleteAmenity(id);
+
+        res.status(200).json({
+            message: `Amenity with id ${deletedAmenityId} was deleted.`
+        });
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+export default router;
